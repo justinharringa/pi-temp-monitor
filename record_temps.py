294666dfd3959385e7229cc4818184f2e690ae92
Justin Harringa <justin@harringa.com>
2015-12-08 13:11:39 -0300

    Add loop for now

--- record_temps.py
+++ record_temps.py
@@ -1,6 +1,9 @@
 from w1thermsensor import W1ThermSensor
+import time
 
-for sensor in W1ThermSensor.get_available_sensors():
-    f = open('mytemps','w')
-    f.write('%s:%.2f\n' % (sensor.id, sensor.get_temperature()))
-    f.close()
+while True:
+    for sensor in W1ThermSensor.get_available_sensors():
+        f = open('mytemps','w')
+        f.write('%s:%.2f\n' % (sensor.id, sensor.get_temperature()))
+        f.close()
+    time.sleep(60)
