f1b9681260e9d2c556c2c6b9dbfad8ab816a5a14
Justin Harringa <justin@harringa.com>
2015-12-08 13:03:26 -0300

    Write to file

--- record_temps.py
+++ record_temps.py
@@ -1,4 +1,6 @@
 from w1thermsensor import W1ThermSensor
 
 for sensor in W1ThermSensor.get_available_sensors():
-    print("Sensor %s has temperature %.2f" % (sensor.id, sensor.get_temperature()))
+    f = open('mytemps','w')
+    f.write('%s:%.2f\n' % (sensor.id, sensor.get_temperature()))
+    f.close()
