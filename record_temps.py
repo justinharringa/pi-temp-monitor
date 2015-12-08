138b5fc33a3d3b8bda27fba0e305ee15cb9f2680
Justin Harringa <justin@harringa.com>
2015-12-08 13:14:28 -0300

    Append to file instead of overwrite

--- record_temps.py
+++ record_temps.py
@@ -3,7 +3,7 @@ import time
 
 while True:
     for sensor in W1ThermSensor.get_available_sensors():
-        f = open('mytemps','w')
+        f = open('mytemps','a')
         f.write('%s:%.2f\n' % (sensor.id, sensor.get_temperature()))
         f.close()
     time.sleep(60)
