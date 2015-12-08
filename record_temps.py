74b39a0d55d51cdf35b03ae12cac0fe56ac13f8c
Justin Harringa <justin@harringa.com>
2015-12-08 18:09:20 -0300

    Remove newline from print statement

--- record_temps.py
+++ record_temps.py
@@ -6,4 +6,4 @@ for sensor in W1ThermSensor.get_available_sensors():
     sensor_id = sensor.id
     temperature = sensor.get_temperature()
     now_isoformat = datetime.now(tzlocal()).isoformat()
-    print('%s,%s,%.3f\n' % (now_isoformat, sensor_id, temperature))
+    print('%s,%s,%.3f' % (now_isoformat, sensor_id, temperature))
