819aa280094756ee3e76c060cdf29ca17d001903
Justin Harringa <justin@harringa.com>
2015-12-08 17:32:18 -0300

    Add formatted datetime and refactor
    Add with statement for file object

--- record_temps.py
+++ record_temps.py
@@ -1,9 +1,13 @@
 from w1thermsensor import W1ThermSensor
+from datetime import *
+from dateutil.tz import *
 import time
 
 while True:
     for sensor in W1ThermSensor.get_available_sensors():
-        f = open('mytemps','a')
-        f.write('%s:%.2f\n' % (sensor.id, sensor.get_temperature()))
-        f.close()
+        sensor_id = sensor.id
+        temperature = sensor.get_temperature()
+        now_isoformat = datetime.now(tzlocal()).isoformat()
+        with open('mytemps','a') as temperature_file:
+            temperature_file.write('%s,%s,%.3f\n' % (now_isoformat, sensor_id, temperature))
     time.sleep(60)
