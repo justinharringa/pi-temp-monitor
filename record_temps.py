b94cbd2f4a312b600bebae90d95c8f9c7becc077
Justin Harringa <justin@harringa.com>
2015-12-08 17:59:08 -0300

    Remove loop to use in cron

--- record_temps.py
+++ record_temps.py
@@ -1,13 +1,10 @@
 from w1thermsensor import W1ThermSensor
 from datetime import *
 from dateutil.tz import *
-import time
 
-while True:
-    for sensor in W1ThermSensor.get_available_sensors():
-        sensor_id = sensor.id
-        temperature = sensor.get_temperature()
-        now_isoformat = datetime.now(tzlocal()).isoformat()
-        with open('mytemps','a') as temperature_file:
-            temperature_file.write('%s,%s,%.3f\n' % (now_isoformat, sensor_id, temperature))
-    time.sleep(60)
+for sensor in W1ThermSensor.get_available_sensors():
+    sensor_id = sensor.id
+    temperature = sensor.get_temperature()
+    now_isoformat = datetime.now(tzlocal()).isoformat()
+    with open('mytemps','a') as temperature_file:
+        temperature_file.write('%s,%s,%.3f\n' % (now_isoformat, sensor_id, temperature))
